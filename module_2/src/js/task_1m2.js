5b5acded7e4f095a3a66411db189d6d46e50a5d1
ekaterina8aleksa <ekaterina.palad87@gmail.com>
2019-10-31 20:41:29 +0200

    module 2 task 1

---
+++ module_2/src/js/task_1m2.js
@@ -0,0 +1,14 @@
+'use strict';
+const nameArr = ['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong'];
+const numberArr = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
+
+function logItems(arr = []) {
+  const arreyLength = arr.length;
+  
+  for (let i = 0; i < arreyLength; i += 1) {
+    console.log(`Position: ${i + 1} - ${arr[i]}`);
+  }
+}
+
+logItems(nameArr);
+logItems(numberArr);
