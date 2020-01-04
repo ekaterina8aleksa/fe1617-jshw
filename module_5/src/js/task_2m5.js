89cd44c61ebce246d5ce937a4a4dc8e2ff1f8ed2
ekaterina8aleksa <ekaterina.palad87@gmail.com>
2020-01-04 06:01:25 +0200

    module 5 add task 3

--- module_5/src/js/task_2m5.js
+++ module_5/src/js/task_2m5.js
@@ -7,7 +7,7 @@ class User {
     this.followers = followers;
   }
 
-  getInfo() {
+   getInfo() {
     console.log(`User ${this.name} is ${this.age} years old and has ${this.followers} followers`);
   }
 }
