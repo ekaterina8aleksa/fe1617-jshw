c06834a5d1eff32f77bc0b47297d728bc26c499f
ekaterina8aleksa <ekaterina.palad87@gmail.com>
2020-03-10 05:15:16 +0200

    8 module arrows slider v1.0

--- module_8/src/js/project1_m8.js
+++ module_8/src/js/project1_m8.js
@@ -4,6 +4,10 @@ const galleryRef = document.querySelector('.js-gallery');
 const overlay = document.querySelector('.js-lightbox');
 const pictureItem = document.querySelector('.lightbox__image');
 const closeBtn = document.querySelector('button[data-action="close-lightbox"]');
+let i = 0;
+let currentSlide;
+let bigImg;
+let bigImgAlt;
 
 /*const createGallery = gallery => {
 const galleryUnit = document.createElement('li');
@@ -31,10 +35,19 @@ const createGalleryList = galleryItems.reduce((acc, item) => {
 //galleryRef.append(createGalleryList);
 galleryRef.insertAdjacentHTML('afterbegin', createGalleryList);
 
+const prevBtn = document.createElement('button');
+prevBtn.classList.add('lightbox__button', 'btn-prev');
+
+const nextBtn = document.createElement('button');
+nextBtn.classList.add('lightbox__button', 'btn-next');
+
+overlay.append(prevBtn, nextBtn);
+
 function overlayOpen() {
   event.preventDefault();
   window.addEventListener('keydown', onEsc);
-
+  window.addEventListener('keydown', onArrowRigth);
+  window.addEventListener('keydown', onArrowLeft);
   if (event.target.nodeName !== 'IMG') return;
   else {
     overlay.classList.add('is-open');
@@ -46,6 +59,8 @@ function overlayOpen() {
 function overlayClose() {
   event.preventDefault();
   window.removeEventListener('keydown', onEsc);
+  window.removeEventListener('keydown', onArrowRigth);
+  window.removeEventListener('keydown', onArrowLeft);
   overlay.classList.remove('is-open');
   pictureItem.src = '';
   pictureItem.alt = '';
@@ -62,9 +77,55 @@ function overlayClickClose(event) {
     overlayClose();
   }
 }
+// пытаюсь определить на какую картинку клацнули, проверяю ее в диапазоне, чтобы использовать ее как стартовую точку
+const onLeftPrev = () => {
+  currentSlide = event.target;
+  bigImg = event.target.parentNode.href;
+  bigImgAlt = event.target.alt;
+  if (// задаю диапазон
+    galleryItems.indexOf(currentSlide) < galleryItems.length &&
+    galleryItems.indexOf(currentSlide) >= 0
+  ) {
+    i -= 1;
+    pictureItem.src = galleryItems[i].href;//bigImg[i];
+    pictureItem.alt = galleryItems[i].alt;//bigImgAlt[i];
+  }
+  else i = 0;
+};
 
+const onRightNext = () => {
+  currentSlide = event.target;
+  bigImg = event.target.parentNode.href;
+  bigImgAlt = event.target.alt;
+  if (
+    galleryItems.indexOf(currentSlide) >= 0 &&
+    galleryItems.indexOf(currentSlide) < galleryItems.length
+  ) {
+    i += 1;
+    pictureItem.src = galleryItems[i].href;//bigImg[i];
+    pictureItem.alt = galleryItems[i].alt;//bigImgAlt[i];
+  }
+  else i = 0;
+};
+
+function onArrowRigth(event) {
+  if (event.code === 'ArrowRight') {
+    onRightNext();
+  }
+}
+
+function onArrowLeft(event) {
+  if (event.code === 'ArrowLeft') {
+    onLeftPrev();
+  }
+}
 galleryRef.addEventListener('click', overlayOpen);
 closeBtn.addEventListener('click', overlayClose);
 overlay.addEventListener('click', overlayClickClose);
+prevBtn.addEventListener('click', onLeftPrev);
+nextBtn.addEventListener('click', onRightNext);
 
-
+/*function keyHandler(event){
+console.log("code: " +event.code);
+}
+ window.addEventListener('keydown', keyHandler);*/
